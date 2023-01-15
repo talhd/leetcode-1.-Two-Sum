e7a70006f3d1e77a86f5115c163cfa66dcddc7c0
Tal Hadad <33845935+talhd@users.noreply.github.com>
2023-01-15 20:34:50 +0200

    Create Valid-Parentheses.java

---
+++ Valid-Parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+       for (int i =0;i<s.length();i++){
+           char index = s.charAt(i);
+           if (index == '(' || index == '[' ||index == '{'){
+               stack.push(index);
+               continue;
+
+           }else {
+               if(stack.empty()){
+                   return false;
+               }else {
+                   char pivot = stack.pop();
+                   if(index == ')' && (pivot == '[' || pivot == '{')){
+                        return false;
+                   }else if(index == '}' && (pivot == '[' || pivot == '(')){
+                       return false;
+                   } else if (index == ']' && (pivot == '(' || pivot == '{')) {
+                       return false;
+                   }
+               }
+           }
+       }
+        return stack.empty();
+    }
+}
